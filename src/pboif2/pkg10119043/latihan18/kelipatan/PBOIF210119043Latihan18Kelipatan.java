dcc13bcbcef66aeca1f3f1c7205b705d39c20ca7
FermiNA29 <58778153+FermiNA29@users.noreply.github.com>
2020-10-30 13:39:01 +0700

    tugas pbo program kelipatan

---
+++ src/pboif2/pkg10119043/latihan18/kelipatan/PBOIF210119043Latihan18Kelipatan.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119043.latihan18.kelipatan;
+
+/**
+ *
+ * @author 
+ * NAMA      : FERMI NAUFAL AKBAR
+ * NIM       : 10119043
+ * KELAS     : IF2
+ * DESKRIPSI : Program kelipatan
+ */
+public class PBOIF210119043Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double angka = 3.5;
+        for (double i=3.5; i <= 35; i+=3.5) {
+            System.out.println(i);
+        }
+        System.out.println();
+        System.out.println("---------------------------------");
+        System.out.println("Developed by : Fermi Naufal Akbar");
+    }
+    
+}
